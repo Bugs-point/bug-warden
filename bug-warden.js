99b1ed09a3651042ea3fed9984951219dd938952
muhammad-usman-shaikh <muhammadusmanshaikh00@gmail.com>
2023-11-03 10:25:11 +0500

    http logs enabled

---
+++ bug-warden.js
@@ -0,0 +1,20 @@
+function BugWarden(req, res, next) {
+  const startTime = Date.now();
+  res.on("finish", () => {
+    const elapsedTime = Date.now() - startTime;
+    const logDetails = `
+      IP: ${req.ip}
+      Timestamp: [${new Date().toUTCString()}]
+      Method: "${req.method} ${req.originalUrl} HTTP/${req.httpVersion}"
+      Status: ${res.statusCode}
+      Content-Length: ${res.getHeader("content-length") || 0}
+      Referrer: "${req.get("referrer") || "-"}"
+      User-Agent: "${req.get("user-agent")}"
+      Response-Time: ${elapsedTime}ms
+    `;
+    console.log(logDetails);
+  });
+  next();
+}
+
+module.exports = BugWarden;
