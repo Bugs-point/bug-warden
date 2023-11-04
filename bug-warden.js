1533c62f242f00dd3e4f70a89c0141ecbfea4f97
wahajansari08 <wahajansari08@gmail.com>
2023-11-04 07:08:31 +0500

    Database Created

--- bug-warden.js
+++ bug-warden.js
@@ -1,21 +1,97 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../../bugwarden.db');
+
+
+function dbMigrate(){
+  db.exec(`
+    CREATE TABLE api_monitoring_logs (
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      ip TEXT NOT NULL,
+      timestamp DATETIME NOT NULL,
+      method TEXT NOT NULL,
+      original_url TEXT NOT NULL,
+      http_version TEXT NOT NULL,
+      status TEXT NOT NULL,
+      contentlength TEXT NOT NULL,
+      referrer TEXT NOT NULL,
+      user_agent TEXT NOT NULL,
+      response_time TEXT NOT NULL
+    );
+  `)
+}
+
+
 function BugWarden(req, res, next) {
+  dbMigrate();
+
   const startTime = Date.now();
+  // listener
   res.on("finish", () => {
-    console.log("my directory + ", __dirname);
     const elapsedTime = Date.now() - startTime;
+    const ip = req.ip
+    const timestamp = new Date().toUTCString()
+    const method = req.method 
+    const originalUrl = req.originalUrl
+    const httpVersion = `HTTP/${req.httpVersion}` 
+    const status = res.statusCode
+    const contentLength = `${res.getHeader("content-length") || 0}`
+    const refferrer = `${req.get("referrer") || "-"}`
+    const userAgent = `${req.get("user-agent")}`
+    const responseTime = `${elapsedTime}ms`
+
+
     const logDetails = `
-      IP: ${req.ip}
-      Timestamp: [${new Date().toUTCString()}]
-      Method: "${req.method} ${req.originalUrl} HTTP/${req.httpVersion}"
-      Status: ${res.statusCode}
-      Content-Length: ${res.getHeader("content-length") || 0}
-      Referrer: "${req.get("referrer") || "-"}"
-      User-Agent: "${req.get("user-agent")}"
-      Response-Time: ${elapsedTime}ms
+      IP: ${ip}
+      Timestamp: [${timestamp}]
+      Method: ${method}
+      OriginalUrl: ${originalUrl}
+      HttpVersion: ${httpVersion}
+      Status: ${status}
+      Content-Length: ${contentLength}
+      Referrer: ${refferrer}
+      User-Agent: "${userAgent}"
+      Response-Time: ${responseTime}
     `;
+
+    // Inserting logs into db
+    dbInsert( ip, timestamp, method, originalUrl, httpVersion, status, contentLength, refferrer, userAgent, responseTime);
+
     console.log(logDetails);
   });
+  
+  
   next();
+}
+
+function dbInsert(
+  ip, timestamp, method, originalUrl, httpVersion, status, contentLength, refferrer, userAgent, responseTime
+){
+  db.exec(`
+  INSERT INTO your_table_name (
+    ip, 
+    timestamp, 
+    method, 
+    original_url,
+    http_version,
+    status, 
+    contentlength,
+    referrer, 
+    user_agent, 
+    response_time
+    )
+  VALUES (
+    '${ip}', 
+    '${timestamp}', 
+    '${method}', 
+    '${originalUrl}', 
+    '${httpVersion}', 
+    '${status}, 
+    '${contentLength}'
+    '${refferrer}'
+    '${userAgent}'
+    '${responseTime}'
+    );
+  `)
 }
 
 module.exports = BugWarden;
